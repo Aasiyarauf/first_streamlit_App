73ae8b467c0103a29e2b312615b182cad7a88bc8
Aasiyarauf <98042418+Aasiyarauf@users.noreply.github.com>
2023-03-07 17:47:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('my parents new healty dinner')
 streamlit.header('breakfast menu')
-streamlit.text('🥣 🥑🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 avocada toast')
