8ae149251e315635ab6399ce59f7c226e3ab2f77
Aasiyarauf <98042418+Aasiyarauf@users.noreply.github.com>
2023-03-27 16:29:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,10 +44,8 @@ try:
     else:
         back_from_function=get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-    
-    #newfunction create
-    
 streamlit.header("MY FRUIT LOAD LIST CONTAIN:")
+#snowflake related function
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from  fruit_load_list")
