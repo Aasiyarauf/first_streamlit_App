d912c5c8e50b881abca95cf5b5d22f57062d730b
Aasiyarauf <98042418+Aasiyarauf@users.noreply.github.com>
2023-03-28 12:41:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,22 +17,15 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
-
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-#.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
- #my_fruit_list = my_fruit_list.set_index('Fruit')
-# streamlit.dataframe(my_fruit_list)
-
 
 #fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avacoda','Strawberry'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.dataframe(fruits_to_show)
 
-#streamlit.header("fruityvice fruit advice")
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response)
+streamlit.dataframe(my_fruit_list)
+
+
+
+streamlit.header("fruityvice fruit advice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
