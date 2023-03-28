5ee2da4f9c09eb82750dca7a6e974a75b2f62b8b
Aasiyarauf <98042418+Aasiyarauf@users.noreply.github.com>
2023-03-28 12:09:01 +0500

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,1 +1,32 @@
 
+
+import streamlit
+import pandas
+import requests
+#import snowflake.connector
+#from urllib.error import urlerror
+
+
+streamlit.title('my parents new healty dinner')
+streamlit.header('breakfast menu')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 avocada toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe( my_fruit_list)
+
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+ #my_fruit_list = my_fruit_list.set_index('Fruit')
+ #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avacoda','Strawberry'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
