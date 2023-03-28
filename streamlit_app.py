feee664ad6fd14dc2bcaa66e47b4d46a7fffe4b1
Aasiyarauf <98042418+Aasiyarauf@users.noreply.github.com>
2023-03-28 12:10:16 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
-
 import streamlit
 import pandas
 import requests
-import snowflake.connector
-from urllib.error import urlerror
+#import snowflake.connector
+#from urllib.error import urlerror
 
 
 streamlit.title('my parents new healty dinner')
@@ -25,41 +24,6 @@ streamlit.dataframe( my_fruit_list)
  #my_fruit_list = my_fruit_list.set_index('Fruit')
  #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-#fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avacoda','Strawberry'])
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
- #function create
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-   
-streamlit.header("fruityvice fruit advice")
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("please select the fruit to get the infoemation.")
-#streamlit.write('The user entered ', fruit_choice)
-    else:
-        back_from_function=get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-#streamlit.header("MY FRUIT LOAD LIST CONTAIN:")
-#snowflake related function
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from  fruit_load_list")
-         return my_cur.fetchall()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-
-#add button
-if streamlit.button('get fruit load list')
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-   my_data_rows =get_fruit_load_list()
-   streamlit.dataframe(my_data_rows)
-    #streamlit.header("MY FRUITLIST CONTAIN:")
-#streamlit.text("Hello from Snowflake:")
-   
-
-
-    
+fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avacoda','Strawberry'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
